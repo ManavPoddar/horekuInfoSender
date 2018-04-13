b706fa65edbdf6afde64f8eb0ccfd0956db76319
ManavPoddar <manavpoddar17@gmail.com>
2018-04-13 19:10:41 +0530

    asa

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+var clientCount = 0
+
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+   // Socket has connected, increase socket count
+    clientCount++
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  clientCount--
+});
+
+
+
+/*var mysql = require('mysql')
+// Let’s make node/socketio listen on port 3000
+var io = require('socket.io').listen(process.env.PORT||3000)
+// Define our db creds
+var db = mysql.createConnection({
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12232016',
+    password: 'RYPIjLf67H',
+    database: 'sql12232016'
+})
+ 
+// Log any errors connected to the db
+db.connect(function(err){
+    if (err) console.log(err)
+})
+
+var clientCount = 0
+ 
+io.sockets.on('connection', function(socket){
+	console.log("Client connected...");
+    // Socket has connected, increase socket count
+    clientCount++
+ 
+    socket.on('disconnect', function() {
+		console.log("Player disconnected...");
+        // Decrease the socket count on a disconnect, emit
+        clientCount--
+    })
+ 
+    socket.on('senddata', function(data){
+        db.query('INSERT INTO UserInformation (IMSI, IMEI, NetworkOperator, CellId, Lattitude, Longitude, NetworkType,Available_Wifi_List,Saved_Wifi_List) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?)', [data.IMSI, data.IMEI, data.network_operator, data.cell_id, data.lattitude, data.longitude, data.network_type, data.wifi_available, data.wifi_saved])
+		//db.query('INSERT INTO UserInformation (WifiListAvailable) VALUES (?)', data.wifi_list_available)
+		//db.query('INSERT INTO UserInformation (WifiListSaved) VALUES (?)', data.wifi_list_saved)
+		console.log("data sent...")
+    })
+})
+
+*/
